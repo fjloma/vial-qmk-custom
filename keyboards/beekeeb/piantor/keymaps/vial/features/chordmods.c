35189234680b40cbd417cc45550a6c110134ff43
FJavier Loma <fjavier.loma@gmail.com>
2023-12-26 19:07:34 +0100

    chordmoc v1

--- keyboards/beekeeb/piantor/keymaps/vial/features/chordmods.c
+++ keyboards/beekeeb/piantor/keymaps/vial/features/chordmods.c
@@ -26,16 +26,31 @@ chordmod_t _chordmod_defs[NR_CHORDMODS] = {
   {4, chordb, modsb, pressedb, 0, 0, false},
 };
 
+void update_mods(chordmod_t *chord) {
+	if (!chord->issued) return;
+	
+	set_mods(0);
+	uint8_t mods = 0;
+	int c = 0;
+	for (int i = 0; i < chord->size; i++) {
+		if (chord->pressed[i]) {
+#ifdef CONSOLE_ENABLE
+	//uprintf("CHORD release: 0x%04X 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, keycode_layer, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+	uprintf("CHORD mod  %d\n", i);
+#endif			
+			mods |= MOD_BIT(chord->mod[i]);
+			c++;
+			if (c==chord->count-1) break; // !! Discard last mod
+		}
+	}
+	
+	register_mods(mods);
+}
+
 int is_chordmod_key(chordmod_t *chord, uint16_t keycode, keyrecord_t* record) {
-    uint16_t keycode_layer = keycode;//keymap_key_to_keycode(COMBO_ONLY_FROM_LAYER, record->event.key);
-    #ifdef CONSOLE_ENABLE
-        uprintf("kc: 0x%04X 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, keycode_layer, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
-    #endif
-    for (int i; i < chord->size; i++) {
+    uint16_t keycode_layer = keymap_key_to_keycode(COMBO_ONLY_FROM_LAYER, record->event.key);
+    for (int i = 0; i < chord->size; i++) {
         if (keycode_layer == chord->key[i]) {
-            #ifdef CONSOLE_ENABLE
-                uprintf("CHORD: 0x%04X 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, keycode_layer, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
-            #endif
             return i;
         }
     }
@@ -43,13 +58,14 @@ int is_chordmod_key(chordmod_t *chord, uint16_t keycode, keyrecord_t* record) {
 }
 
 void pressed_key(chordmod_t *chord, int key_index, uint16_t keycode) {
+
     if (!chord->pressed[key_index]) {
         chord->count++;
         chord->pressed[key_index] = keycode;
 
         if (chord->issued) {
             // already on chord, just add the mod
-            register_mods(chord->mod[key_index]);
+            update_mods(chord);
         } else {
             chord->timer = timer_read32();
         }
@@ -67,27 +83,25 @@ void cancel_chord(chordmod_t *chord) {
         }
     }
 
+	set_mods(0);
+
     chord->issued = false;
     chord->timer = 0;
     chord->count = 0;
-    uint8_t mods = 0;
     for (int i = 0; i < chord->size; i++) {
         if (chord->pressed[i]) {
-            mods |= MOD_BIT(chord->mod[i]);
             chord->pressed[i] = 0;
         }
     }
-    unregister_mods(mods);
-
 }
 
 void released_key(chordmod_t *chord, int key_index, uint16_t keycode) {
 
     if (chord->pressed[key_index]) {
         if (chord->issued) {
-            // already on chord, remove the mod
-            if (chord->pressed[key_index]) unregister_mods(chord->mod[key_index]);
-        }
+        } else {
+            tap_code16(keycode);
+		}
 
         chord->count--;
         if (chord->count<0) chord->count = 0;
@@ -97,13 +111,14 @@ void released_key(chordmod_t *chord, int key_index, uint16_t keycode) {
             // already on chord, remove the mod
             if (chord->count<2) {
                 cancel_chord(chord);
-            }
+            } else {
+				update_mods(chord);
+			}
         } else {
             chord->timer = 0;
         }
     }
 }
-
 
 
 bool chordmods_process(uint16_t keycode, keyrecord_t* record) {
@@ -114,8 +129,11 @@ bool chordmods_process(uint16_t keycode, keyrecord_t* record) {
         for (int comboid = 0; comboid < NR_CHORDMODS; comboid++) {
             chordmod_t* chord = &_chordmod_defs[comboid];
             int key_index = is_chordmod_key(chord, keycode, record);
+			#ifdef CONSOLE_ENABLE
+                uprintf("CHORD press: 0x%04X 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, key_index, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+            #endif
+            
             if (key_index != -1) {
-                tap_code16(KC_FIND);
                 // pressed combo key
                 ret = false;
                 pressed_key(chord, key_index, keycode);
@@ -127,8 +145,11 @@ bool chordmods_process(uint16_t keycode, keyrecord_t* record) {
         for (int comboid = 0; comboid < NR_CHORDMODS; comboid++) {
             chordmod_t* chord = &_chordmod_defs[comboid];
             int key_index = is_chordmod_key(chord, keycode, record);
+			#ifdef CONSOLE_ENABLE
+                uprintf("CHORD release: 0x%04X 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, key_index, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+            #endif
+
             if (key_index != -1) {
-                tap_code16(KC_CUT);
                 if (!chord->issued)
                     ret = false;
                 released_key(chord, key_index, keycode);
@@ -140,22 +161,27 @@ bool chordmods_process(uint16_t keycode, keyrecord_t* record) {
     return ret;
 }
 
+bool check_chord_issued(chordmod_t *chord) {
+    chord->issued = false;
+	if (chord->count == 2) {
+		//last two key pressed must be consecutive
+		for (int i = chord->size-1; i > 0; i--) {
+			if (chord->pressed[i]) {
+				if (chord->pressed[i-1])
+					chord->issued = true;
+				break;
+			}
+		}
+	}
+	return chord->issued;
+}
+
 void chordmods_task(void) {
     for (int comboid = 0; comboid < NR_CHORDMODS; comboid++) {
         chordmod_t* chord = &_chordmod_defs[comboid];
-        if (!chord->issued && timer_elapsed32(chord->timer) > 5000) {
-            if (chord->count>=2) {
-                chord->issued = true;
-                uint8_t mods = 0;
-                int c = 0;
-                for (int i = 0; i < chord->size; i++) {
-                    if (chord->pressed[i]) {
-                        mods |= MOD_BIT(chord->pressed[i]);
-                        c++;
-                        //if (c==chord->size) break; // !! Discard last mod
-                    }
-                }
-                register_mods(mods);
+        if (!chord->issued && timer_elapsed32(chord->timer) > 50) {
+            if (check_chord_issued(chord)) {
+                update_mods(chord);
             } else {
                 // not enough keys after the combo term
                 // we cancel all and tap the last code
